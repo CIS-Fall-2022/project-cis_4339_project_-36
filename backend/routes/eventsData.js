043bb89b2bfd3eecb95793d743864ec294d4e376
Danny Hayashi <72723716+DanielHayashi-tech@users.noreply.github.com>
2022-10-06 14:07:41 -0500

    small clean up

--- backend/routes/eventsData.js
+++ backend/routes/eventsData.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 //importing data model schemas
-let {eventdata} = require("../models/models");
+let { eventdata } = require("../models/models");
 
 //GET all entries
 router.get("/", (req, res, next) => { 
