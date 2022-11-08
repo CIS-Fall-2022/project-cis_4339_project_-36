8b7c579e43d832f649cd06a568711b92a40e5469
ericchen100 <ericchen100@yahoo.com>
2022-11-07 23:23:37 -0600

    Changed for organization name showing up top right

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -48,7 +48,7 @@
           background: linear-gradient(250deg, #C8102E 70%, #efecec 50.6%);
         "
       >
-        <h1 class="mr-20 text-3xl text-white">Dataplatform</h1>
+        <h1 class="mr-20 text-3xl text-white">{{currentOrg[0].orgName}}</h1>
       </section>
       <div>
         <router-view></router-view>
@@ -58,8 +58,23 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "App",
+  data() {
+    return {
+      currentOrg: []
+    }
+  },
+  created() {
+    let apiURL = 'http://localhost:3000/orgData/current_org';
+    axios.get(apiURL).then(res => {
+      this.currentOrg = res.data;
+    }).catch(error => {
+      console.log(error)
+    });
+  },
 };
 </script>
 
