638824ae58b04895d4a29ef5800f3655bfc2ef59
ericchen100 <ericchen100@yahoo.com>
2022-11-16 01:49:32 -0600

    Merge branch 'main' of https://github.com/CIS-Fall-2022/project-cis_4339_project_-36

--- frontend/src/components/dashboard.vue
+++ frontend/src/components/dashboard.vue
@@ -9,8 +9,12 @@
         <div class="ml-10">
           <h2 class="text-2xl font-bold text-red-700">List of Events and Number of Attendees (Clients) in the Last 2 Months</h2>
         </div>
-        <!-- Table of list of events and number of attendees in the last 2 months -->
         <br>
+        <div>
+          <canvas id="eventsChart" width="400" height="400" ></canvas>
+        </div>
+        <br>
+        <!-- Table of list of events and number of attendees in the last 2 months -->
         <table class="min-w-full shadow-md rounded">
           <!-- Field names for the table -->
           <thead class="bg-gray-50 text-xl">
@@ -29,39 +33,77 @@
         </table>
       </div>
     </div>
-    <br>
-      <div>
-        <canvas id="myChart" width="400" height="400"></canvas>
-      </div>
   </main>
 </template>
 
 
 <script>
-import {Chart} from "chart.js";
 import axios from "axios";
+import { Chart, registerables } from 'chart.js';
+Chart.register(...registerables);
 
 export default {
   data() {
     return {
-      eventsData: []
+      eventsData: [],
+      labels: [],
+      numAttendees: []
     }
   },
   created() {
-    let apiURL = 'http://localhost:3000/eventData/event-data';
+    // getting eventsData
+    let apiURL = `http://localhost:3000/eventData/event-data`;
     axios.get(apiURL).then(res => {
       this.eventsData = res.data;
     }).catch(error => {
       console.log(error)
     });
   },
+  mounted() {
+    this.fetchData();
+
+    const ctx = document.getElementById('eventsChart');
+
+    const eventsChart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: this.labels,
+        datasets: [{
+          label: 'Number of Attendees (Clients)',
+          data: this.numAttendees,
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)'
+          ],
+          borderColor: [
+            'rgba(255, 99, 132, 1)'
+          ],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+
+    // call the eventsChart
+    eventsChart;
+  },
   methods: {
     // function to count the number of attendees in the attendees array from an event
     countAttendees: function(eventAttendees) {
       // return the length of array with attendees of an event
       return eventAttendees.length;
+    },
+    async fetchData() {
+      const url = `http://localhost:3000/eventData/event-data`;
+      const response = await axios.get(url);
+      this.labels = response.data.map((event) => event.eventName);
+      this.numAttendees = response.data.map((event) => event.attendees.length);
     }
-  },
+  }
 };
-
 </script>
